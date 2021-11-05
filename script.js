2996547ebe3929f79e20d8c81e781f6a256ed6cd
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-11-05 12:15:59 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const quizForm = document.querySelector(".quiz-form");
 const bazingaBtn = document.querySelector("#bazinga-btn");
 const outputEl = document.querySelector("#output");
+const outputOne = document.querySelector("#output-one");
+
 
 const correctAnswers = ["CALTECH", "soft" , "asymmetry" , "cello" , "pasadena"];
 
@@ -15,6 +17,12 @@ function calculateScore(){
         index = index + 1;
     }
     outputEl.innerText = "Your score is " + score;
+    
+    if(score < 3){
+        outputOne.innerText = "Better luck next time 😔";
+    } else{
+        outputOne.innerText = "You are a true BBT fan 🥰";
+    }
 
 }
 
